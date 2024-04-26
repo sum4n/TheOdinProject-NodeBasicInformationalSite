231e02d091d2fa1626a31c7bab4c8f95288c633d
Suman Das <sum4ndas@gmail.com>
2024-04-26 12:52:39 +0530

    Rewrite using Express

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 const path = require("path");
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  // generate file path with req object
-  let filePath = path.join(
-    __dirname,
-    "public",
-    req.url == "/" ? "index.html" : req.url
-  );
-
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      // If file not found error serve the 404.html page
-      if (err.code == "ENOENT") {
-        fs.readFile(
-          path.join(__dirname, "public", "404.html"),
-          "utf-8",
-          (err, data) => {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        );
-      } else {
-        res.writeHead(500);
-        res.end(`Server error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-server.listen(8000, () => console.log("Server running on port 8000..."));
+app.get("/:address", (req, res) => {
+  res.sendFile(path.join(__dirname + `/public/${req.params.address}.html`));
+});
+
+// handle file not found error
+app.use((err, req, res, next) => {
+  // console.log(err);
+  res.status(404).sendFile(path.join(__dirname + "/public/404.html"));
+});
+
+// Use static files with /filename.type
+// app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
